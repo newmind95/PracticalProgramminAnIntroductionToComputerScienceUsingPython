a741614541d0186845b46c3bedeeecfb031aa2b2
newmind95 <javakris95@gmx.com>
2021-08-28 11:46:08 +0300

    Solutions of the exercises

---
+++ Chapter8/listmethods.py
@@ -0,0 +1,9 @@
+colors = ['red', 'orange', 'green']
+colors.extend(['black', 'blue'])
+print(colors)
+colors.append('purple')
+print(colors)
+colors.insert(2, 'yellow')
+print(colors)
+colors.remove('black')
+print(colors)
